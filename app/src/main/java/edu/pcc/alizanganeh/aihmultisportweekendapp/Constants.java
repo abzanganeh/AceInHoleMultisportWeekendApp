7a11c387b848f7d37e99e590fe2771a19ec7ad1a
Alireza <alireza.barzinzanganeh@pcc.edu>
2017-11-06 12:18:39 -0800

    add scroll to FAQ and What to bring and registration

--- app/src/main/java/edu/pcc/alizanganeh/aihmultisportweekendapp/Constants.java
+++ app/src/main/java/edu/pcc/alizanganeh/aihmultisportweekendapp/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static final String FIREBASE_REGISTER_CHILD_FIRSTNAME = "reg_firstname";
     public static final String FIREBASE_CHILD_REGISTERS = "registers";
     public static final String FIREBASE_CHILD_VOLUNTEER = "volunteer";
+    public static final int MIN_PROGRESS_VALUE = 5;
+    public static final int MAX_PROGRESS_VALUE = 20;
 }
